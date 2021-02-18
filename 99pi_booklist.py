198345e927052e84d57009abb2653ee44543c326
Lindsey <parkinson.lindsey@protonmail.com>
2021-02-18 11:18:51 +0100

    functions working and call on one another

--- 99pi_booklist.py
+++ 99pi_booklist.py
@@ -14,12 +14,7 @@ from datetime import datetime
 
 
 
-url = 'https://99percentinvisible.org/episodes/?view_option=list'
-#response = requests.get(url, timeout = 2)
-#response.status_code # 200 is good 
-
-#soup = BeautifulSoup(response.content, "html.parser")
-#one_episode = soup.find("article", class_ = "list-block post episode")
+#url = 'https://99percentinvisible.org/episodes/?view_option=list'
 
 
 def get_episodes(url):
@@ -65,20 +60,18 @@ def get_episodes(url):
     return episodes_onepage
 
 
-# get information for all episode pages
-
 #Scrape all pages or set page limit
 #max_pages = soup.find("a",{"class": "page-numbers"}).find_next_siblings("a")[-1].get("data-page-number")
-max_pages = 4
+#max_pages = 4
 
-#can make this better. Don't need a second time
-#cols = ["date", "episode_number", "title", "episode_link"]
-#df = pd.DataFrame(columns = cols)
 
 def get_all_episodes(max_pages):
     link_first_part = "https://99percentinvisible.org/episodes/page/"
     link_2_part = "/?view_option=list"
-
+    
+    cols = ["date", "episode_number", "title", "episode_link"]
+    episodes_multipage = pd.DataFrame(columns = cols)
+    
     for page in tqdm(range(1, int(max_pages) + 1)):
         """Make the urls dynamic"""
         url = (
@@ -86,18 +79,15 @@ def get_all_episodes(max_pages):
             + str(page)
             + link_2_part
         )
-        response = requests.get(url, timeout=15)
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        episodes_onepage = get_episodes(soup)  # function created earlier
-        df = df.append(episodes_onepage, ignore_index=True)
-
+        episodes_multipage = episodes_multipage.append(get_episodes(url), ignore_index=True)# = get_episodes(url).append(episodes_onepage, ignore_index=True)
         sleep(0.6) # to keep up with human speed we need to slow down program
-    return df
+    
+    return episodes_multipage
 
 # episode description extracted from each link
 # appended to existing dataframe
-def get_description(df):
+def get_description(max_pages):
+    df = get_all_episodes(max_pages)
     description = []
     for link in tqdm(df["episode_link"]):
         response2 = requests.get(link, timeout=15)
